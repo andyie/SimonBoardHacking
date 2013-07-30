d815c3332fef69e16e1ed03bca03ef53f5a02b56
andyie <andrewierickson@gmail.com>
2013-07-30 05:18:44 -0700

    updating blinkLED

--- BlinkLED/BlinkLED.ino
+++ BlinkLED/BlinkLED.ino
@@ -3,9 +3,9 @@
 
 void repeat()
 {
-  turn_on_light(1);
+  turn_on_light(upper_left_light);
   wait_this_many_seconds(0.5);
 
-  turn_off_light(1);
+  turn_off_light(upper_left_light);
   wait_this_many_seconds(0.5);
 }
