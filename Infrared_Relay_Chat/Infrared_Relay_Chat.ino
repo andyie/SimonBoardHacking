56ffe6fa222d5e4bb669f73eec98fae6413554a5
andyie <andrewierickson@gmail.com>
2013-07-30 11:24:38 -0700

    adding relay chat project

---
+++ Infrared_Relay_Chat/Infrared_Relay_Chat.ino
@@ -0,0 +1,48 @@
+#include <SimonBoardHacking.h>
+#include <Ir.h>
+#include <Utility.h>
+#include <Events.h>
+
+hear_event(event_upper_left_light_on) {
+  turn_on_light(upper_left_light);
+  announce_event(event_upper_left_light_on);
+}
+
+hear_event(event_upper_left_light_off) {
+  turn_off_light(upper_left_light);
+  announce_event(event_upper_left_light_off);
+}
+
+hear_event(event_upper_right_light_on) {
+  turn_on_light(upper_right_light);
+  announce_event(event_upper_right_light_on);
+}
+
+hear_event(event_upper_right_light_off) {
+  turn_off_light(upper_right_light);
+  announce_event(event_upper_right_light_off);
+}
+
+hear_event(event_lower_right_light_on) {
+  turn_on_light(lower_right_light);
+  announce_event(event_lower_right_light_on);
+}
+
+hear_event(event_lower_right_light_off) {
+  turn_off_light(lower_right_light);
+  announce_event(event_lower_right_light_off);
+}
+
+hear_event(event_lower_left_light_on) {
+  turn_on_light(lower_left_light);
+  announce_event(event_lower_left_light_on);
+}
+
+hear_event(event_lower_left_light_off) {
+  turn_off_light(lower_left_light);
+  announce_event(event_lower_left_light_off);
+}
+
+hear_string() {
+  announce_string(received_string);
+}
